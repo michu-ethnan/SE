edf1dbbdc0f748b1973d437b56fd562c3bd543ab
Abi <tekasimeon22@gmail.com>
2022-04-28 16:51:04 +0200

    SE created

---
+++ src/main/java/com/deosite/tests/i18n/Bundle_se_SE.java
@@ -0,0 +1,147 @@
+package com.deosite.tests.i18n;
+
+import com.deosite.tests.model.account.NewAddressForm;
+import com.deosite.tests.model.login.LoginForm;
+import com.deosite.tests.model.login.DifferentLoginForm;
+import com.deosite.tests.model.order.Personal;
+import com.deosite.tests.model.order.Company;
+import com.deosite.tests.model.order.WithoutEmailAddress;
+import com.deosite.tests.model.register.RegisterForm;
+
+import java.util.ListResourceBundle;
+
+public class Bundle_se_SE extends ListResourceBundle {
+
+    @Override
+    protected Object[][] getContents() {
+        return new Object[][]{
+                // Labels
+                {"capacity (l) label", "pojemność (l)"},
+                {"capacity (ml) label", "pojemność (ml)"},
+
+                // Products
+                {"mug", "Taza"},
+                {"cup", "hrnek"},
+
+                {"configurable product", "kubek palette"},
+
+                // Number of products in the cart
+                {"greater", "Din korg (1)"},
+                {"smaller", "Din korg (2)"},
+
+                // Categories
+
+                //Headers
+                {"account header", "Konto"},
+                {"Ogród header", "Trädgård "},
+                {"Jadalnia header", "Matsal "},
+                {"Kuchnia header", "Kök "},
+                {"Łazienka i garderoba header", "Hushåll "},
+                {"Dekoracje header", "Textil "},
+                {"Świat dziecka header", "Barnens värld "},
+                {"Nowości header", "Nyheter "},
+                {"Wyprzedaż header", "Rea "},
+                {"Marki header", "Märken"},
+
+
+
+                // Alerts
+                {"added to cart", "Produkten har lagts till i kundvagnen"},
+                {"address saved", "Adressen har sparats"},
+                {"address deleted", "Address deleted"},
+
+                // Messages
+                {"success order message", "Dziękujemy, Twoje zamówienie zostało złożone!"},
+                {"additional success info", "Zamówienie jest kompletowane, o wysyłce poinformujemy drogą mailową."},
+                {"the cart is empty message", "Korgen är tom"},
+                {"login button text", "Logga in"},
+                // Objects
+                {"person", new Personal(
+                        "test@test.pl",
+                        "Test",
+                        "Test-Test",
+                        "Testowa 1",
+                        "Testowo",
+                        "11-111",
+                        "123456789"
+
+                )},
+                {"personFromPortugal", new Personal(
+                        "test@test.pl",
+                        "Test",
+                        "Test-Test",
+                        "Testowa 1",
+                        "Testowo",
+                        "11-111",
+                        "123456789"
+                )},
+                {"personFromIreland", new Personal(
+                        "test@test.pl",
+                        "Test",
+                        "Test-Test",
+                        "Testowa 1",
+                        "Testowo",
+                        "11-111",
+                        "123456789"
+                )},
+                {"company", new Company(
+                        "test@test.pl",
+                        "Test",
+                        "Test-Test",
+                        "Testowa 1",
+                        "Testowo",
+                        "11-111",
+                        "123456789",
+                        "Testowa sp. z o.o",
+                        "1234567891"
+
+                )},
+                {"withoutEmailAddress", new WithoutEmailAddress(
+                        "Test",
+                        "Test-Test",
+                        "Testowa 1",
+                        "Testowo",
+                        "11-111",
+                        "123456789"
+                )},
+                {"login", new LoginForm(
+                        "tester-email-tester@yandex.com",
+                        "Test12345!"
+                )},
+                {"login to order_1", new LoginForm(
+                        "abisimeon22@yandex.com",
+                        "Test12345!"
+                )},
+                {"login to order_2", new LoginForm(
+                        "abisimeon222@yandex.com",
+                        "Test12345!"
+                )},
+                {"login to order_3", new LoginForm(
+                        "abisimeon223@yandex.com",
+                        "Test12345!"
+                )},
+                {"login to see order", new LoginForm(
+                        "tekasimeon00@gmail.com",
+                        "Test12345!"
+                )},
+                {"differentEmail", new DifferentLoginForm(
+                        "abisimeon223@yandex.com",
+                        "Test12345!"
+                )
+                },
+                {"newAddress", new NewAddressForm(
+                        "Salomea",
+                        "Kowalska",
+                        "Testowa 1",
+                        "Testowo",
+                        "00-000",
+                        "0000000000"
+                )},
+                {"register", new RegisterForm(
+                        "Test12345!",
+                        "Test",
+                        "Test-Test"
+                )}
+        };
+    }
+}
